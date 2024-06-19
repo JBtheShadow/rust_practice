22c5a4eabdef4bdc6d3b67265053f831f1a08445
JBtheShadow <jbtheshadow@gmail.com>
2024-06-19 15:03:45 -0300

    JB - using threads

---
+++ learning/threads/src/main.rs
@@ -0,0 +1,51 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    example();
+    example_waiting_threads();
+    passing_values_to_threads();
+}
+
+fn example() {
+    // When the main thread ends it also ends every other
+    // related thread regardless of their state
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+fn example_waiting_threads() {
+    // To prevent that you can grab the handle and wait for completion
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
+
+fn passing_values_to_threads() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+}
